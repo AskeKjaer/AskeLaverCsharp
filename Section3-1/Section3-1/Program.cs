090683029e3b6ee8fb95c4d3ebf6bf8b1a9b5ac6
AskeKjaer <aske@ryskov.dk>
2020-05-13 23:13:44 +0200

    Added the worst leap year calculator ever

--- Section3-1/Section3-1/Program.cs
+++ Section3-1/Section3-1/Program.cs
@@ -4,9 +4,48 @@ namespace Section3_1
 {
     class Program
     {
+        private static int YearValidator(string input) 
+        {
+            int Year = 0;
+            {
+                if ((int.TryParse(input, out Year)))
+                {
+                    Year = int.Parse(input);
+                    if (Year >= 0 && Year < 10000)
+                    {
+                        return Year;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid year. Using 2020.");
+                        return 2020;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid year. Using 2020.");
+                    return 2020;
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int CurrentYear = 0;
+            string CurrentYearInput;
+            bool IsYear = false;
+            Console.WriteLine("What year is it?");
+            CurrentYearInput = Console.ReadLine();
+            CurrentYear = YearValidator(CurrentYearInput);
+            if (CurrentYear % 4 == 0 && (CurrentYear % 100 != 0 || CurrentYear % 400 == 0))
+            {
+                Console.WriteLine($"{CurrentYear} is a Leap Year!");
+            }
+            else
+            {
+                Console.WriteLine($"{CurrentYear} is not a Leap Year!");
+            }
+            Console.WriteLine("Press Any Key to Exit");
+            Console.Read();
         }
     }
 }
